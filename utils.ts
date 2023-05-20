6307b7ca97e2f409bf8334db545d37f65bfcf0e8
tahhash <tahh.hashh@gmail.com>
2023-05-20 21:42:46 +0100

    s

--- utils.ts
+++ utils.ts
@@ -1,16 +1,6 @@
-
+// utils.ts
 
 export function currency(price: number, fraction: number = 2): string {
-  const { publicRuntimeConfig } = getConfig() as {
-    publicRuntimeConfig: { currency: string };
-  };
-
-  const formatCurrency = new Intl.NumberFormat(undefined, {
-    style: 'currency',
-    currency: publicRuntimeConfig.currency,
-    maximumFractionDigits: fraction,
-    minimumFractionDigits: fraction,
-  });
-
-  return formatCurrency.format(price);
+  const formattedPrice = price.toFixed(fraction);
+  return formattedPrice;
 }
