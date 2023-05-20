7c3afe5ec55d8eb13964330bbdee26b1642b23c9
tahhash <tahh.hashh@gmail.com>
2023-05-20 14:50:19 +0100

    Cart

--- src/contexts/AppContext.jsx
+++ src/contexts/AppContext.jsx
@@ -8,28 +8,28 @@ const INITIAL_CART = [
   {
     qty: 1,
     price: 210,
-    slug: "silver-high-neck-sweater",
-    name: "Silver High Neck Sweater",
+    slug: "flow-white",
+    name: "Flow White",
     id: "6e8f151b-277b-4465-97b6-547f6a72e5c9",
     imgUrl:
-      "/assets/images/products/Fashion/Clothes/1.SilverHighNeckSweater.png",
+      "/assets/images/products/Fashion/Shoes/11.FlowWhite.png",
   },
   {
     qty: 1,
     price: 110,
-    slug: "yellow-casual-sweater",
-    name: "Yellow Casual Sweater",
+    slug: "nike-air-white",
+    name: "Nike Air White",
     id: "76d14d65-21d0-4b41-8ee1-eef4c2232793",
     imgUrl:
-      "/assets/images/products/Fashion/Clothes/21.YellowCasualSweater.png",
+      "/assets/images/products/Fashion/Shoes/12.NikeAirWhite.png",
   },
   {
     qty: 1,
     price: 140,
-    slug: "denim-blue-jeans",
-    name: "Denim Blue Jeans",
+    slug: "nike-mint",
+    name: "Nike Mint",
     id: "0fffb188-98d8-47f7-8189-254f06cad488",
-    imgUrl: "/assets/images/products/Fashion/Clothes/4.DenimBlueJeans.png",
+    imgUrl: "/assets/images/products/Fashion/Shoes/15.NikeMint.png",
   },
 ];
 const INITIAL_STATE = {
