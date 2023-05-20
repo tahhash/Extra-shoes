d806f3bcacbc490ae7683198effc5d754267faf2
tahhash <tahh.hashh@gmail.com>
2023-05-20 21:46:19 +0100

    Update utils.ts

--- utils.ts
+++ utils.ts
@@ -1,6 +1,15 @@
-// utils.ts
+import getConfig from 'next/config';
 
 export function currency(price: number, fraction: number = 2): string {
-  const formattedPrice = price.toFixed(fraction);
-  return formattedPrice;
+  const { publicRuntimeConfig } = getConfig() || { publicRuntimeConfig: { showCurrencySymbol: true } };
+
+  const formatCurrency = new Intl.NumberFormat(undefined, {
+    style: 'currency',
+    currency: publicRuntimeConfig.currency,
+    currencyDisplay: publicRuntimeConfig.showCurrencySymbol ? 'symbol' : 'code',
+    maximumFractionDigits: fraction,
+    minimumFractionDigits: fraction,
+  });
+
+  return formatCurrency.format(price);
 }
