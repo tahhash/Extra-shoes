241a9400235351feadb1398fc08278bb6da88bc2
tahhash <tahh.hashh@gmail.com>
2023-05-25 12:42:09 +0100

    navigation

--- src/data/navigations.js
+++ src/data/navigations.js
@@ -1,73 +1,60 @@
-import BabyBoy from "components/icons/BabyBoy";
-import BabyGirl from "components/icons/BabyGirl";
-import Car from "components/icons/Car";
+
 import Dress from "components/icons/Dress";
-import Food from "components/icons/Food";
-import Gift from "components/icons/Gift";
-import Laptop from "components/icons/Laptop";
-import MakeUp from "components/icons/MakeUp";
-import Man from "components/icons/Man";
-import Microphone from "components/icons/Microphone";
-import MotorBike from "components/icons/MotorBike";
-import Pets from "components/icons/Pets";
-import PlantPot from "components/icons/PlantPot";
-import TeddyBear from "components/icons/TeddyBear";
-import Woman from "components/icons/Woman";
 const navigations = [
   {
     icon: Dress,
-    title: "Fashion",
+    title: "الأزياء",
     href: "/fashion",
     menuComponent: "MegaMenu1",
     menuData: {
       categories: [
         {
-          title: "Men's Shoes",
+          title: "أحذية الرجال",
           href: "/product/search/shoes",
           subCategories: [
             {
-              title: "Sneakers",
+              title: "حذاء رياضي",
               href: "/product/search/Sneakers",
               imgUrl: "/assets/images/products/categories/sneaker.png",
             },
             {
-              title: "Sandals",
+              title: "صندل",
               href: "/product/search/Sandals",
               imgUrl: "/assets/images/products/categories/sandal.png",
             },
             {
-              title: "Formal",
+              title: "رسمي",
               href: "/product/search/Formal",
               imgUrl: "/assets/images/products/categories/shirt.png",
             },
             {
-              title: "Casual",
+              title: "عادي",
               href: "/product/search/Casual",
               imgUrl: "/assets/images/products/categories/t-shirt.png",
             },
           ],
         },
         {
-          title: "Women's Shoes",
+          title: "أحذية النساء",
           href: "/product/search/shoes",
           subCategories: [
             {
-              title: "Sneakers",
+              title: "حذاء رياضي",
               href: "/product/search/Sneakers",
               imgUrl: "/assets/images/products/categories/sneaker.png",
             },
             {
-              title: "Sandals",
+              title: "صندل",
               href: "/product/search/Sandals",
               imgUrl: "/assets/images/products/categories/sandal.png",
             },
             {
-              title: "Formal",
+              title: "رسمي",
               href: "/product/search/Formal",
               imgUrl: "/assets/images/products/categories/shirt.png",
             },
             {
-              title: "Casual",
+              title: "عادي",
               href: "/product/search/Casual",
               imgUrl: "/assets/images/products/categories/t-shirt.png",
             },
