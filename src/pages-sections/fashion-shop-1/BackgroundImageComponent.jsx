b335dbc95a099d84ca1e7678a024d48db9307ad7
tahhash <tahh.hashh@gmail.com>
2023-05-25 23:18:25 +0100

    Update BackgroundImageComponent.jsx

--- src/pages-sections/fashion-shop-1/BackgroundImageComponent.jsx
+++ src/pages-sections/fashion-shop-1/BackgroundImageComponent.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Box, Container, Grid } from "@mui/material";
+import { Box, Container} from "@mui/material";
 
 
 const BackgroundImageComponent = () => {
@@ -30,36 +30,42 @@ const BackgroundImageComponent = () => {
         mb: "4rem",
       }}
     >
-    <div
-      style={{
-        backgroundImage: 'url("/homepage.jpg")',
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        backgroundAttachment: 'fixed', // Add this line
-        height: '70vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        position: 'relative',
-      }}
-    >
-      <h2
-        style={{
-          position: "absolute",
-          top: "50%",
-          left: "33.33%",
-          transform: "translate(-50%, -50%)",
-          color: "white",
-          zIndex: 1,
-          fontSize: "36px",
-          fontWeight: "bold",
-          textAlign: 'center',
+      <Box
+        sx={{
+          position: 'relative',
+          width: '100%',
+          height: '70vh',
+          overflow: 'hidden',
         }}
       >
-        اعثر على المقاس المثالي لقدميك
-      </h2>
-    </div>
-
+        <img
+          src="/homepage.jpg"
+          alt="Background Image"
+          style={{
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            height: '100%',
+            width: 'auto',
+          }}
+        />
+        <h2
+          style={{
+            position: 'absolute',
+            top: '50%',
+            left: '33.33%',
+            transform: 'translate(-50%, -50%)',
+            color: 'white',
+            zIndex: 1,
+            fontSize: '36px',
+            fontWeight: 'bold',
+            textAlign: 'center',
+          }}
+        >
+          اعثر على المقاس المثالي لقدميك
+        </h2>
+      </Box>
     </Container>
   );
 };
