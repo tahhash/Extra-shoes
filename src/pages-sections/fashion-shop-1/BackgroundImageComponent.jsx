f9dc1a27900e6437b320e7e84e6ca796b481f0a4
tahhash <tahh.hashh@gmail.com>
2023-05-26 22:22:52 +0100

    Update BackgroundImageComponent.jsx

--- src/pages-sections/fashion-shop-1/BackgroundImageComponent.jsx
+++ src/pages-sections/fashion-shop-1/BackgroundImageComponent.jsx
@@ -27,57 +27,55 @@ const BackgroundImageComponent = () => {
       pt: "1rem",
       mb: "4rem",
     }}>
-
-      <Grid>
-      <div
-      style={{
-        width: '100vw', // Set width to 100vw for full screen width
-        height: '70vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        position: 'relative',
-        overflow: 'hidden', // Ensure content stays within the parent div
-      }}
-    >
-      <div
-        style={{
-          position: 'absolute',
-          top: 0,
-          left: 0,
-          width: '100%',
-          height: '100%',
-          zIndex: -1, // Set z-index to move the background behind other content
-        }}
-      ></div>
-      <h2
-        style={{
-          position: 'absolute',
-          top: '50%',
-          left: '50%',
-          transform: 'translate(-50%, -50%)',
-          color: 'white',
-          zIndex: 1,
-          fontSize: '36px',
-          fontWeight: 'bold',
-          textAlign: 'center',
-        }}
-      >
-        اعثر على المقاس المثالي لقدميك
-      </h2>
-      <div
-        style={{
-          backgroundImage: 'url("/homepage.jpg")',
-          backgroundSize: '100vw',
-          backgroundPosition: 'center',
-          width: '100%',
-          height: '100%',
-        }}
-      ></div>
-    </div>
+      <Grid container justifyContent="center">
+        <div
+          style={{
+            width: '100vw', // Set width to 100vw for full screen width
+            height: '70vh',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            position: 'relative',
+            overflow: 'hidden', // Ensure content stays within the parent div
+          }}
+        >
+          <div
+            style={{
+              position: 'absolute',
+              top: 0,
+              left: 0,
+              width: '100%',
+              height: '100%',
+              zIndex: -1, // Set z-index to move the background behind other content
+            }}
+          ></div>
+          <h2
+            style={{
+              position: 'absolute',
+              top: '50%',
+              left: '50%',
+              transform: 'translate(-50%, -50%)',
+              color: 'white',
+              zIndex: 1,
+              fontSize: '36px',
+              fontWeight: 'bold',
+              textAlign: 'center',
+            }}
+          >
+            اعثر على المقاس المثالي لقدميك
+          </h2>
+          <div
+            style={{
+              backgroundImage: 'url("/homepage.jpg")',
+              backgroundSize: 'cover',
+              backgroundPosition: 'center',
+              width: '100%',
+              height: '100%',
+            }}
+          ></div>
+        </div>
       </Grid>
     </Container>
-    
   );
 };
 
