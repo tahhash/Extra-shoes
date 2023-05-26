3cd53d992c02f63e5f277c2742af2e1f11ba28a2
tahhash <tahh.hashh@gmail.com>
2023-05-26 22:59:19 +0100

    Update Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -40,7 +40,7 @@ const StyledContainer = styled(Container)({
 });
 const currentHeight = 44;
 const newHeight = Math.round(currentHeight * 1.5);
-const newWidth = Math.round(newHeight * 2.25);
+const newWidth = Math.round(newHeight * aspectRatio);
 
 
 // ==============================================================
@@ -166,7 +166,7 @@ const Header = ({ isFixed, className, searchInput }) => {
         {/* LEFT CONTENT - LOGO AND CATEGORY */}
         <FlexBox mr={2} minWidth="170px" alignItems="center">
           <Link href="/">
-          <Image height={newHeight} width={newWidth} src="/assets/images/logo2.svg" alt="logo" />
+          <Image height={75} width={113} src="/assets/images/logo2.svg" alt="logo" />
           </Link>
 
           {/* SHOW DROP DOWN CATEGORY BUTTON WHEN HEADER FIXED */}
