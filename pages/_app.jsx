104f87a779b0bb520e989974a369d5831365ba06
tahhash <tahh.hashh@gmail.com>
2023-05-31 21:26:50 +0100

    Update _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -34,7 +34,7 @@ const App = (props) => {
         <meta charSet="utf-8" />
         <meta
           name="description"
-          content="React Next.js ecommerce template. Build SEO friendly Online store, delivery app and Multivendor store"
+          content=""
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
